15b2a5044e2d965ff3a51c4f574da24afea2768d
Kira Zaytseva <kirazaytseva@192.168.1.34>
2022-08-11 14:59:36 +0300

    feat: add check brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let openBrackets = Object.fromEntries(bracketsConfig);
+  let closeBrackets = Object.keys(openBrackets).reduce((acc, rec) => {
+    return {...acc, [openBrackets[rec]]: rec};
+  }, {});
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let symb = str[i];
+    if (typeof openBrackets[symb] !== 'undefined') {
+      stack.push(symb);
+      // continue;
+    }
+    if (typeof closeBrackets[symb] !== 'undefined') {  
+      const closingSymbol = stack.pop();  
+      if (openBrackets[closingSymbol] !== symb) { 
+        return false; 
+        break;
+      }  
+    } 
+  }
+
+  return stack.length  === 0;
 }
